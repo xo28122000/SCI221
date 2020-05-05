42c38387c8f94fa04cb00db75f93c278bb74302d
lubuggiano <33555483+lubuggiano@users.noreply.github.com>
2020-05-05 12:55:36 -0700

    Update BST.java

--- Trees/src/BST.java
+++ Trees/src/BST.java
@@ -26,25 +26,7 @@ System.out.println("Congrats! You've added" + val);
             root.left = delete(root.left, val); 
         else if (va; > root.val) 
             root.right = delete(root.right, val); 
-  
-        // if key is same as root's key, then This is the node 
-        // to be deleted 
-        else
-        { 
-            // node with only one child or no child 
-            if (root.left == null) 
-                return root.right; 
-            else if (root.right == null) 
-                return root.left; 
-  
-            // node with two children: Get the inorder successor (smallest 
-            // in the right subtree) 
-            root.key = minValue(root.right); 
-  
-            // Delete the inorder successor 
-            root.right = deleteRec(root.right, root.key); 
-        } 
-  
+        
         return root; 
     } 
             
