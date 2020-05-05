432a852bcecac9494c2c52d5efde04f748f884da
lubuggiano <33555483+lubuggiano@users.noreply.github.com>
2020-05-05 12:51:10 -0700

    Update BST.java

--- Trees/src/BST.java
+++ Trees/src/BST.java
@@ -1,15 +1,29 @@
 public class BST {
     Node root;
     
-    public void add(int val) {
-
+    public void add(int val) { 
+ 
+        if (root == null) { 
+            root = new Node(val); 
+            return root; 
+        } 
+        if (this.val < root.val) 
+            root.left = add(root.left, val); 
+        else if (this.val > root.val) 
+            root.right = add(root.right, val); 
+        return root; 
     }
+         }
 
     public void print(){
-
+System.out.println("Congrats! You've added" + val);
     }
 
     public void delete(int val) {
 
+    }
+    
+     public boolean isEmpty() {
+        return size() == 0;
     }
 }
